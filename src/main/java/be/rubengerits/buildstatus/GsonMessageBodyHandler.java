9ad7b2817aee7b41e443d32374239adf6e3d2802
Ruben Gerits <rubengerits360@gmail.com>
2016-04-12 08:12:15 +0200

    Change date format

--- src/main/java/be/rubengerits/buildstatus/GsonMessageBodyHandler.java
+++ src/main/java/be/rubengerits/buildstatus/GsonMessageBodyHandler.java
@@ -20,56 +20,57 @@ import java.lang.reflect.Type;
 @Consumes(MediaType.APPLICATION_JSON)
 public final class GsonMessageBodyHandler implements MessageBodyWriter<Object>, MessageBodyReader<Object> {
 
-	private static final String UTF_8 = "UTF-8";
+    private static final String UTF_8 = "UTF-8";
 
-	private Gson gson;
+    private Gson gson;
 
-	private Gson getGson() {
-		if (gson == null) {
-			final GsonBuilder gsonBuilder = new GsonBuilder();
-			gson = gsonBuilder.create();
-		}
-		return gson;
-	}
+    private Gson getGson() {
+        if (gson == null) {
+            final GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            gson = gsonBuilder.create();
+        }
+        return gson;
+    }
 
-	@Override
-	public boolean isReadable(Class<?> type, Type genericType, java.lang.annotation.Annotation[] annotations, MediaType mediaType) {
-		return true;
-	}
+    @Override
+    public boolean isReadable(Class<?> type, Type genericType, java.lang.annotation.Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
 
-	@Override
-	public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException {
-		InputStreamReader streamReader = new InputStreamReader(entityStream, UTF_8);
-		try {
-			Type jsonType;
-			if (type.equals(genericType)) {
-				jsonType = type;
-			} else {
-				jsonType = genericType;
-			}
-			return getGson().fromJson(streamReader, jsonType);
-		} finally {
-			streamReader.close();
-		}
-	}
+    @Override
+    public Object readFrom(Class<Object> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, String> httpHeaders, InputStream entityStream) throws IOException {
+        InputStreamReader streamReader = new InputStreamReader(entityStream, UTF_8);
+        try {
+            Type jsonType;
+            if (type.equals(genericType)) {
+                jsonType = type;
+            } else {
+                jsonType = genericType;
+            }
+            return getGson().fromJson(streamReader, jsonType);
+        } finally {
+            streamReader.close();
+        }
+    }
 
-	@Override
-	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-		return true;
-	}
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return true;
+    }
 
-	@Override
-	public long getSize(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-		return -1;
-	}
+    @Override
+    public long getSize(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
 
-	@Override
-	public void writeTo(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
-		OutputStreamWriter writer = new OutputStreamWriter(entityStream, UTF_8);
-		try {
-			getGson().toJson(object, type, writer);
-		} finally {
-			writer.close();
-		}
-	}
+    @Override
+    public void writeTo(Object object, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        OutputStreamWriter writer = new OutputStreamWriter(entityStream, UTF_8);
+        try {
+            getGson().toJson(object, type, writer);
+        } finally {
+            writer.close();
+        }
+    }
 }
